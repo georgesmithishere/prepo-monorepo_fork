d9b0d2bf3baa17282a16f7123e4f26907b0cad15
George Smith <george@prepo.io>
2022-09-26 23:08:16 +0300

    feat: update

--- apps/frontend/core-dapp/jest.config.ts
+++ apps/frontend/core-dapp/jest.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 
 const fromRoot = (directory: string): string => path.join(__dirname, directory)
 process.env.TZ = 'UTC'
-console.log(2)
+console.log(23)
 
 const config: Config.InitialOptions = {
   ...base,

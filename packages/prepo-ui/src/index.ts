373c1c4bb579f454e81632c1f9caedc517a2a81c
George Smith <george@prepo.io>
2022-09-27 00:25:01 +0300

    feat: ui2

--- packages/prepo-ui/src/index.ts
+++ packages/prepo-ui/src/index.ts
@@ -1,6 +1,6 @@
 export * from './components'
 export * from './common-utils'
 export * from './themes/themes.types'
-export const d = 123
+export const d = 1234
 // Theme files export 123
 export * as coreDappTheme from './themes/core-dapp'
